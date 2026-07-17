05d719c527e5cc804bd25c53528e33a70932d725
gitgoggles <ruckojr@hotmail.com>
2026-07-17 20:17:32 +0100

    adjusted federate-credential to allow for immutable IDs

--- infra/bootstrap.sh
+++ infra/bootstrap.sh
@@ -2,9 +2,11 @@
 set -euo pipefail
 
 APP_NAME="bicep-deploy-example"
-GITHUB_ORG="gitgoggles"
+GITHUB_OWNER="gitgoggles"
 GITHUB_REPO="bicep-deploy-example"
 SUBSCRIPTION_ID="$(az account show --query id --output tsv)"
+GITHUB_OWNER_ID="$(gh api "repos/${GITHUB_ORG}/${GITHUB_REPO}" --jq '.owner.id')"
+GITHUB_REPO_ID="$(gh api "repos/${GITHUB_ORG}/${GITHUB_REPO}" --jq '.id')"
 
 APP_ID="$(az ad app create --display-name "$APP_NAME" --query appId --output tsv)"
 
@@ -16,10 +18,10 @@ az role assignment create --assignee-object-id "$OBJECT_ID" --assignee-principal
 
 cat > federated-credential.json <<EOF
 {
-  "name": "github-main",
+  "name": "github-production",
   "issuer": "https://token.actions.githubusercontent.com",
-  "subject": "repo:${GITHUB_ORG}/${GITHUB_REPO}:ref:refs/heads/main",
-  "description": "Deployments from the main branch",
+  "subject": "repo:${GITHUB_OWNER}@${GITHUB_OWNER_ID}/${GITHUB_REPO}@${GITHUB_REPO_ID}:environment:production",
+  "description": "Deployments from the production environment",
   "audiences": [
     "api://AzureADTokenExchange"
   ]
