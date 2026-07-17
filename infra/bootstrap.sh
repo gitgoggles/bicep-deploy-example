67827fe4a7e644ee010bf9f1da15a60dda50350f
gitgoggles <ruckojr@hotmail.com>
2026-07-17 21:49:24 +0100

    automated repo variable publishing

--- infra/bootstrap.sh
+++ infra/bootstrap.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# azure cli and github cli need to be logged in.
+# the script will fail early if you are not.
+az account show
+gh auth status
+
 APP_NAME="bicep-deploy-example"
 GITHUB_OWNER="gitgoggles"
 GITHUB_REPO="bicep-deploy-example"
+ENVIRONMENT='production'
+
 SUBSCRIPTION_ID="$(az account show --query id --output tsv)"
 GITHUB_OWNER_ID="$(gh api "repos/${GITHUB_ORG}/${GITHUB_REPO}" --jq '.owner.id')"
 GITHUB_REPO_ID="$(gh api "repos/${GITHUB_ORG}/${GITHUB_REPO}" --jq '.id')"
@@ -12,16 +19,16 @@ APP_ID="$(az ad app create --display-name "$APP_NAME" --query appId --output tsv
 
 az ad sp create --id "$APP_ID"
 
-OBJECT_ID="$(az ad sp show --id "$APP_ID" --query id --output tsv)"
+SP_OBJECT_ID="$(az ad sp show --id "$APP_ID" --query id --output tsv)"
 
-az role assignment create --assignee-object-id "$OBJECT_ID" --assignee-principal-type ServicePrincipal --role Contributor --scope "/subscriptions/$SUBSCRIPTION_ID"
+az role assignment create --assignee-object-id "$SP_OBJECT_ID" --assignee-principal-type ServicePrincipal --role Contributor --scope "/subscriptions/$SUBSCRIPTION_ID"
 
 cat > federated-credential.json <<EOF
 {
-  "name": "github-production",
+  "name": "github-${ENVIRONMENT}",
   "issuer": "https://token.actions.githubusercontent.com",
-  "subject": "repo:${GITHUB_OWNER}@${GITHUB_OWNER_ID}/${GITHUB_REPO}@${GITHUB_REPO_ID}:environment:production",
-  "description": "Deployments from the production environment",
+  "subject": "repo:${GITHUB_OWNER}@${GITHUB_OWNER_ID}/${GITHUB_REPO}@${GITHUB_REPO_ID}:environment:${ENVIRONMENT}",
+  "description": "Deployments from the ${ENVIRONMENT} environment",
   "audiences": [
     "api://AzureADTokenExchange"
   ]
@@ -35,3 +42,8 @@ TENANT_ID="$(az account show --query tenantId --output tsv)"
 printf 'AZURE_CLIENT_ID=%s\n' "$APP_ID"
 printf 'AZURE_TENANT_ID=%s\n' "$TENANT_ID"
 printf 'AZURE_SUBSCRIPTION_ID=%s\n' "$SUBSCRIPTION_ID"
+
+gh variable set AZURE_CLIENT_ID --body "${APP_ID}" --repo "${GITHUB_OWNER}/${GITHUB_REPO}" --env "${ENVIRONMENT}"
+gh variable set AZURE_TENANT_ID --body "${TENANT_ID}" --repo "${GITHUB_OWNER}/${GITHUB_REPO}" --env "${ENVIRONMENT}"
+gh variable set AZURE_SUBSCRIPTION_ID --body "${SUBSCRIPTION_ID}" --repo "${GITHUB_OWNER}/${GITHUB_REPO}" --env "${ENVIRONMENT}"
+gh variable list --repo "${GITHUB_OWNER}/${GITHUB_REPO}" --env "${ENVIRONMENT}"
